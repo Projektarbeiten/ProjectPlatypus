1830436066c563443ee78b603d1eca70e6658272
GCoder <gauthiergaukelneo@gmail.com>
2023-03-24 01:08:36 +0100

    header strukturiert, datenbank verbindung gebaut, login vergrößert

---
+++ phpFunctions/databaseConnection.php
@@ -0,0 +1,21 @@
+<?php
+// Creates a connection to the Database
+class DataConnection {
+  public function buildConnection($connpath){
+    $inipath = ("$connpath/config/app.ini");
+    $ini = parse_ini_file($inipath);
+    header('Content-type: text/html; charset=utf-8');
+    $host = $ini['db_host'];
+    $username = $ini['db_user'];
+    $user_pwd = $ini['db_password'];
+    $database = $ini['db_name'];
+
+    try {
+      $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $user_pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+      return $pdo;
+
+    } catch (PDOException $ex) {
+      die(json_encode(array('outcome' => false, 'message' => "Unable to connect to the Database. Try if the Database exists :) ")));
+    }
+  }
+}
