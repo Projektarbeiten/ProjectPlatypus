1f570bb5fc8cfa32f501378ee69b10b6225c5b76
Markus <markus.blumenstock13@gmail.com>
2023-05-16 08:11:32 +0200

    erste Surchergebnispage hinzugefügt

---
+++ searchpageresult.php
@@ -0,0 +1,43 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Suchergebnis</title>
+</head>
+<body>
+ <!-- Header -->
+ <?php
+    require("header.php");?>
+<main class="searchpage">
+    <?php
+    
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-1-2">
+            <h2 class="kategorie-name">Kategorie 1</h2>
+                        <a href="">
+                            <img class="kategorie-bild" src="./img/testBild.png" alt="Undefined picture">
+                        </a>
+                        <p class="kategorie-text">Kategorie Text 1</p>
+            </div>
+            <div class="col-1-2"></div>
+            <div class="col-1-2"></div>
+            <div class="col-1-2"></div>
+
+        </div>
+    </div>
+</main>
+</body>
+<!-- Footer -->
+<?php
+    require("footer.php");
+    ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+</html>
