4d931ec1ead38c21ead3d91ee6ff475f85cc505c
Gauthier Gaukel <g.gaukel@scolution.de>
2023-06-15 16:15:50 +0200

    index css gefixt

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
-
-
-require ('./phpFunctions/databaseConnection.php');
-require ("./phpFunctions/util.php");
+require('./phpFunctions/databaseConnection.php');
+require("./phpFunctions/util.php");
 $conn = buildConnection("./");
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
 	<meta charset='utf-8'>
@@ -21,35 +19,36 @@ $conn = buildConnection("./");
 <body>
 	<div class="container">
 		<div class="row">
-				<div class="col-6">
-					<div id="login-form">
-						<p>Bitte den Code eingeben</p>
-						<form method="post">
-							<input type="text" id="code" name='code'></input>
-							<div style="display: flex">
-								<button class="boxrechts" id="back">Zurück</button>
-								<button type="submit" class="boxlinks" id="eingabe">Eingabe</button>
-							</div>
-						</form>
-					</div>
+			<div class="col-6">
+				<div id="code-login-form">
+					<p>Bitte den Code eingeben</p>
+					<form method="post">
+						<input type="password" id="code" name='code'></input>
+						<div style="display: flex">
+							<button class="boxrechts access-code-button">Zurück</button>
+							<button type="submit" class="boxlinks access-code-button">Eingabe</button>
+						</div>
+					</form>
 				</div>
+			</div>
 		</div>
 		<?php
-			if(isset($_POST['code'])){
-				$conn = buildConnection('.');
-				if(checkCode($_POST['code'],$conn)){
-					$_SESSION['access_token'] = true;
-					header("Location: home");
-				}
+		if (isset($_POST['code'])) {
+			$conn = buildConnection('.');
+			if (checkCode($_POST['code'], $conn)) {
+				$_SESSION['access_token'] = true;
+				header("Location: home");
 			}
+		}
 		?>
 	</div>
 	<div id="login-footer">>
-	<?php
+		<?php
 		require("footer.php");
-	?>
+		?>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-	<!--<script src="javascript/indexLogin.js"></script>-->
+	<script src="javascript/indexLogin.js"></script>
 </body>
+
 </html>
