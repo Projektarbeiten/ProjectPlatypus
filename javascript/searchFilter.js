78facea4adf7c279d70a8985f899d2174debddc9
Markus <markus.blumenstock13@gmail.com>
2023-06-24 01:02:44 +0200

    begin Filter

---
+++ javascript/searchFilter.js
@@ -0,0 +1,8 @@
+"use strict";
+let products = []
+let searchString = "hfusehfks"
+for (let product of products) {
+    if(!product[1].includes(searchString)){
+        // element ausblenden
+    };
+}
