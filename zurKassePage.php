69f5419ab925a2d53c3ce6b1e793d9b21c9e91eb
Kevin Junker <k.junker@gerhard-schubert.de>
2023-06-27 10:47:40 +0200

    Page und CSS aktualisiert

--- zurKassePage.php
+++ zurKassePage.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+require("./phpFunctions/databaseConnection.php");
+require("./phpFunctions/util.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$conn = buildConnection(".");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,62 +25,70 @@
 	require("header.php");
 	?>
 
+	<div class="container">
     <br>
-    <div class="unterteilungen">
+    <div class="bestelldaten">
         <h3>1 Versandadresse</h3>
-        <div class="box_kasse_versadd">
-        <p>Hier wird die Versandadresse des Kunden dargestellt.</p>
+        <div id="box_versadd">
+		<p>Max Mustermann</p>
+		<p>Mustermannstr. 1</p>
+		<p>70173 Stuttgart, Baden-Württemberg </p>
+		<p>Deutschland</p>
         </div>
     </div>
     <br>
     <hr>
     <br>
-    <div class="unterteilungen">
+    <div class="bestelldaten">
         <h3>2 Zahlungsart</h3>
-        <div class="box_kasse_zahlart">
-        <p>Hier wird die angegebene Zahlungsart angezeigt.</p>
+        <div id="box_zahlart">
+        <p>Bankeinzugskonto endet auf ****</p>
         </div>
     </div>
     <hr>
     <br>
 
-    <div class="unterteilungen">
+    <div class="bestelldaten">
     <h3>3 Artikel und Versand überprüfen</h3>
     </div>
     <br>
-    <div class="box_kasse_u3">
+    <div class="box_check_art_vers">
         <div class="lief_vers">
             <h4>Lieferung:</h4>
-            <p class="dotted-box-lief">Datum</p>
+            <p id="dotted-box-lief">Datum</p>
         </div>
         <br>
         <div style="display: flex;">
-            <div class="solid-box">
+            <div id="solid-box">
             <img src="[PRODUKTBILD-URL]" alt="Produktbild">
             </div>
-            <p class="dotted-box" style="width: 60%;">Artikelbeschreibung</p>
-            <br>
-            <p class="dotted-box-preis">Preis</p>
-            <p class="dotted-box-anzahl">Anzahl</p>
+            <p id="dotted-box-artbeschreibung">Artikelbeschreibung</p>
         </div>
+		<div style="display: flex;">
+			<p id="dotted-box-preis">Preis</p>
+			<p id="dotted-box-anzahl">Anzahl</p>
+		</div>
         <br>
         <div class="lief_vers">
             <h4>Versandart:</h4>
-            <p class="dotted-box-vers">Beschreibung der Versandart</p>
+            <p id="dotted-box-vers">Beschreibung der Versandart</p>
         </div>
     </div>
 
-    <div class="box_kasse_u3">
-    <div class="box_kasse_u3_zent">
-        <button class="checkout-btn" style="margin-left: 5%; width: 15%;">Jetzt kaufen</button>
+    <div class="box_check_art_vers">
+    <div id="box_check_art_vers_zent">
+		<a href="" id="checkout-btn" style="margin-left: 5%; width: 15%;">Jetzt kaufen</a><!-- TODO: Verlinkung einfügen -->
         <h4 style="margin-left: 10%;">Gesamtbetrag:</h4>
-        <p class="dotted-box" style="margin-left: 5%;">Summe</p>
+        <p id="dotted-box-summe" style="margin-left: 5%;">Summe</p>
         <br>
     </div>
         <div style="text-align: center;">
-        <p class="agb">Mit dem Klick auf "Zur Kasse" akzeptieren Sie unsere AGB.</p>
+		<a href="" id="agb">Mit dem Klick auf "Zur Kasse" akzeptieren Sie unsere AGB.</a><!-- TODO: Verlinkung einfügen -->
         </div>
     </div>
+	<br>
+	<a href="" id="back-btn" style="margin-left: 5%; width: 15%;">Zurück zum Warenkorb</a><!-- TODO: Verlinkung einfügen -->
+	</div>
 
 	<!-- Footer -->
 	<?php
